0c50f53b99938f54300ae8468b8e9b4e4869c176
Ben Russell <br@x-plugins.com>
2014-12-06 19:04:50 +1000

    Preparing to cleanup mime_types code.

---
+++ plugin/source/mime_types.cpp
@@ -0,0 +1,9 @@
+//
+//  mime_types.cpp
+//  x-httpd
+//
+//  Created by Ben on 6/12/2014.
+//
+//
+
+#include "mime_types.h"
---
+++ plugin/source/mime_types.h
@@ -0,0 +1,14 @@
+//
+//  mime_types.h
+//  x-httpd
+//
+//  Created by Ben on 6/12/2014.
+//
+//
+
+#ifndef __x_httpd__mime_types__
+#define __x_httpd__mime_types__
+
+#include <iostream>
+
+#endif /* defined(__x_httpd__mime_types__) */
