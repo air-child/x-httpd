e3cd071bb381aec84818fd967091dff29f297fbf
Ben Russell <br@x-plugins.com>
2014-12-11 03:38:44 +1000

    Echo hostname and local IPv4 address on CLI startup!

--- plugin/source/main_cli.cpp
+++ plugin/source/main_cli.cpp
@@ -9,6 +9,8 @@
 #include <iostream>
 
 
+#include "global_defs.h"
+
 #include "x_httpd.h"
 
 
@@ -16,19 +18,34 @@ int main(int argc, const char * argv[])
 {
 
 	// insert code here...
-	std::cout << "x-httpd-cli 14.12.07.1819\n";
-	
+	std::cout << XHTTPD_SERVER_MESSAGE << "\n";
 	printf(" built: %s %s\n", __DATE__, __TIME__);
 	
 	
-	std::cout << "  spawning server..\n";
+	
+	
+	
+		char caDbg[1024];
+	
+		char caHostName[255];
+		gethostname(caHostName, 255);
+		
+		struct hostent *host_entry;
+		host_entry = gethostbyname( caHostName );
+		
+		char *caLocalIP;
+		caLocalIP = inet_ntoa( *(struct in_addr *)*host_entry->h_addr_list);
+		
+		printf( "x-httpd: hostname: %s ip: %s\n", caHostName, caLocalIP );
+
+	
+	
+	
+	std::cout << "Spawning server..\n";
 	x_httpd *httpd = new x_httpd( 1312 );
-	
-	std::cout << "  running..\n";
-	httpd->run_cli();
+	httpd->run_cli(); //runs forever.
 	
 	
-	std::cout << "finished.\n";
     return 0;
 }
 
