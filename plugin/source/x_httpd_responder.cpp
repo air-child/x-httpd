5b4127c61d13b6603001ada88b9658d06b0a6278
Ben Russell <br@x-plugins.com>
2014-12-12 04:37:54 +1000

    Adds working /set/ handler. Needs sensible JSON packet return data.

--- plugin/source/x_httpd_responder.cpp
+++ plugin/source/x_httpd_responder.cpp
@@ -100,7 +100,92 @@ void x_httpd_responder__dref_get::eat( x_httpd_request *request ){
 }
 
 
+
+
+
 void x_httpd_responder__dref_set::eat( x_httpd_request *request ){
+
+
+	printf("response__dref_set.eat()\n");
+	
+	request->response.setContentType("text/plain");
+	
+	
+	bool valid_request = true;
+	
+	
+	std::string payload = "response__dref_set.eat: ";
+	std::string sCmdMode = ""; //define dataref type
+	std::string sNewValue = ""; //last item on URL
+	
+	size_t max_x = request->requestTokens.size();
+	if( max_x < 5 ){
+		payload = "Invalid set call: Not enough tokens.";
+		valid_request = false;
+	}else{
+		sCmdMode = request->requestTokens[2];
+		sNewValue = request->requestTokens[ max_x - 1 ];
+	}
+	
+	
+	if( valid_request ){
+		//reconstruct the command string
+		//skip first three tokens (<blank>/cmd/mode/sim/operation/foo)
+		//The last token is the value we're sending into the dataref.
+		std::string sCmdString = "";
+		for( size_t x=3; x < max_x-1; x++ ){			
+			sCmdString += request->requestTokens[x];
+			if( x < max_x-2 ){
+				sCmdString += "/";
+			}
+		}
+		
+		//we now have a reconstructed command string
+		#if XPLM200
+			printf("XPL mode\n");
+		#else
+			printf("CLI mode\n");
+		#endif
+		
+		//exec according to mode.
+		if( "float" == sCmdMode ){			
+			payload = "set float: " + sCmdString;
+			
+			#if XPLM200
+				float tmpVal = atof( sNewValue.c_str() );
+				XPLMSetDataf( XPLMFindDataRef( sCmdString.c_str() ), tmpVal );
+			#endif
+			
+		}else if( "int" == sCmdMode ){
+			payload = "set int: " + sCmdString;
+
+			#if XPLM200
+				int tmpVal = atoi( sNewValue.c_str() );
+				XPLMSetDatai( XPLMFindDataRef( sCmdString.c_str() ), tmpVal );
+			#endif
+			
+		}else if( "double" == sCmdMode ){
+			payload = "set double: " + sCmdString;
+
+			#if XPLM200
+				double tmpVal = atof( sNewValue.c_str() );
+				XPLMSetDatad( XPLMFindDataRef( sCmdString.c_str() ), tmpVal );
+			#endif
+			
+		}else{
+			payload = "Invalid set call: Unknown mode: " + sCmdMode;
+			valid_request = false;
+		}
+		
+		
+	}else{
+		//request has been deemed invalid
+	
+		//no actions taken.
+	}
+	
+	request->response.setContentBody( payload.c_str() );
+	request->response.write();
 
 }
 
